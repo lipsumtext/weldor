4bed17f0269037eaf76263e382b00cb3a3007491
Jave Paid <jave.paid@obf.ateneo.edu>
2024-09-23 22:23:16 +0800

    Initialize anagram set file with sample word set

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 var checkWord = require('check-word')
 words = checkWord('en')
 
+var anagramSet = require('./anagram_set.json')
+
 const checkIfWord = function (word) {
     return words.check(word)
+}
+
+const returnColor = function (word) {
+    //pass
 }
 
 module.exports = { checkIfWord }
