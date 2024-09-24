818023b0abd4e514412d210d77559ecf54a8dde3
Jave Paid <jave.paid@obf.ateneo.edu>
2024-09-24 23:18:12 +0800

    Add function that returns corresponding color combination

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-var checkWord = require('check-word')
-words = checkWord('en')
+const checkWord = require('check-word')
+const words = checkWord('en')
 
-var anagramSet = require('./anagram_set.json')
+//const anagramSet = require('./anagram_set.json')
 
-const checkIfWord = function (word) {
+const checkIfWord = (word) => {
     return words.check(word)
 }
 
-const returnColor = function (word) {
-    //pass
+const returnColor = (word, wordSet) => {
+    if (checkIfWord(word)) {
+        let result = '' // Green = G, Yellow = Y, Red = R
+        let charCounter = (w) => {
+            let count = {}
+            for (let c of w) {
+                count[c] = (count[c] || 0) + 1
+            }
+            return count
+        }
+        let wordCharCount = charCounter(word), 
+            wordSetCharCount = charCounter(wordSet[0])
+        for (let c of word) {
+            result += ((wordCharCount[c] && wordSetCharCount[c])
+                ? ((wordCharCount[c] == wordSetCharCount[c]) ? 'G' : 'Y') 
+                : 'R'
+            )
+        }
+        return result
+    } else {
+        return false
+    }
 }
 
-module.exports = { checkIfWord }
+module.exports = { checkIfWord, returnColor }
