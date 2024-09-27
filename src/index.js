e9b5753afe9564ed1303b81808eb46cd20ca14a4
Jave Paid <jave.paid@obf.ateneo.edu>
2024-09-27 23:01:53 +0800

    Add frontend and win condition

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const checkWord = require('check-word')
 const words = checkWord('en')
 
-//const anagramSet = require('./anagram_set.json')
+const anagramSet = require('./anagram_set.json')
 
 const checkIfWord = (word) => {
     return words.check(word)
 }
 
-const returnColor = (word, wordSet) => {
-    if (checkIfWord(word)) {
+const returnColor = (word, wordSet, length) => {
+    if (checkIfWord(word) && word.length == length) {
         let result = '' // Green = G, Yellow = Y, Red = R
         let charCounter = (w) => {
             let count = {}
@@ -29,5 +29,34 @@ const returnColor = (word, wordSet) => {
     } 
     return false
 }
+
+const rl = require('readline-sync')
+
+const gameLoop = () => {
+    let count = 0, requiredLength = 6, winConditionMet = false
+    let resultColors = [], correctWords = []
+    while (count < 6) {
+        if (correctWords.length == 3) {
+            winConditionMet = true
+            break
+        }
+        console.log(resultColors)
+        let answer = rl.question('Please enter a six-letter word: ')
+        if (!(correctWords.includes(answer))) {
+            let result = returnColor(answer, anagramSet['sixLetterSet'][0], requiredLength)
+            if (!result) {
+                continue
+            } else if (result == ('G'.repeat(requiredLength))) {
+                correctWords.push(answer)
+            }
+            resultColors.push(result)
+            count++
+        }
+    }
+    console.log(resultColors)
+    console.log(winConditionMet ? 'You win!' : 'You\'re out of attempts. Sorry :(')
+}
+
+gameLoop()
 
 module.exports = { checkIfWord, returnColor }
