eecd35b22b4d56adb33e00aaf75df54c0fdd2f2d
Jave Paid <jave.paid@obf.ateneo.edu>
2024-11-20 03:48:02 +0800

    Add working row in grid

--- src/components/WordGrid.jsx
+++ src/components/WordGrid.jsx
@@ -1,16 +1,36 @@
 import { useEffect } from "react"
 import { useWeldor } from "../hooks/useWeldor"
+import './WordGrid.styles.css'
+
+const Row = ({ word, boxStatus, isActive }) => {
+    return (
+        <>
+            {isActive && 
+                <div className="container">
+                    {Array.from({ length: 6 }, (_, i) => (
+                        <div key={i} className={boxStatus[i] || 'letter-box'}>
+                            {word[i] || ''}
+                        </div>
+                    ))}
+                </div>
+            }
+        </>
+    )
+}
 
 export const WordGrid = () => {
-    const { guessedWord, handleUserInput } = useWeldor()
+    const { guessedWord, boxStatus, handleUserInput } = useWeldor()
 
     useEffect(() => {
         window.addEventListener('keyup', handleUserInput)
-        
+
         return () => window.removeEventListener('keyup', handleUserInput)
     }, [handleUserInput])
     
     return (
-        <div>{guessedWord}</div>
+        <>
+            <Row word={guessedWord.toUpperCase()} boxStatus={boxStatus} isActive={true}/>
+            <div>{guessedWord}</div>
+        </>
     )
 }
