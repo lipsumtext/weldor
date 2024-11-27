1fa7256e4e9833a9d369c80735e6ad677fdb243f
Jave Paid <jave.paid@obf.ateneo.edu>
2024-11-27 22:43:37 +0800

    Add placeholder text + enhance components structure

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,8 +3,14 @@ import './NavBar.styles.css'
 export const NavBar = ({ setShowRules }) => {
     return (
         <nav className="navbar">
-            <h1>Weldor</h1>
-            <button onClick={() => setShowRules(true)}>Show rules</button>
+            <ul>
+                <li>
+                    <h1>Weldor</h1>
+                </li>
+                <li>
+                    <button onClick={() => setShowRules(true)}>Show rules</button>
+                </li>
+            </ul>
         </nav>
     )
 }
