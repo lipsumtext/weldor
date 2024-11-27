3118bb18265d908f04e5c432ae3eb4d19c1bf3f7
Jave Paid <jave.paid@obf.ateneo.edu>
2024-11-27 22:43:37 +0800

    Add placeholder text + enhance components structure

--- src/components/RulesModal/RulesModal.jsx
+++ src/components/RulesModal/RulesModal.jsx
@@ -3,8 +3,17 @@ import './RulesModal.styles.css'
 export const RulesModal = ({ onClose }) => {
     return (
         <div className="modal">
-            <h1>Rules</h1>
-            <button onClick={onClose}>Close</button>
+            <div className='title-bar'>
+                <button onClick={onClose}>✖</button>
+            </div>
+            <h1>Welcome to Weldor!</h1>
+            <h3>How to play: </h3>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam error obcaecati aperiam voluptatibus in quos non quasi architecto excepturi, dolorum minima pariatur soluta deserunt molestiae dolore accusantium. Illo, dolor perspiciatis?
+            </p>
+            <div className="play-weldor">
+                <button onClick={onClose}>Play Weldor</button>
+            </div>
         </div>
     )
 }
