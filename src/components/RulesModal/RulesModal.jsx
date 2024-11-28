8921ee46ff9ad48241a91f557bbd6e9a4878dda0
Jave Paid <jave.paid@obf.ateneo.edu>
2024-11-28 16:57:53 +0800

    Add content to rules modal

--- src/components/RulesModal/RulesModal.jsx
+++ src/components/RulesModal/RulesModal.jsx
@@ -6,11 +6,28 @@ export const RulesModal = ({ onClose }) => {
             <div className='title-bar'>
                 <button onClick={onClose}>✖</button>
             </div>
-            <h1>Welcome to Weldor!</h1>
-            <h3>How to play: </h3>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam error obcaecati aperiam voluptatibus in quos non quasi architecto excepturi, dolorum minima pariatur soluta deserunt molestiae dolore accusantium. Illo, dolor perspiciatis?
-            </p>
+            <div className="welcome">
+                <h1>Welcome to Weldor!</h1>
+                <p>
+                    Our own twist on the internet classic, Wordle. Test your vocabulary and, more importantly, your anagram skills in this challenging, yet fun, word game.
+                </p>
+            </div>
+            <div className="how-to-play">
+                <h3>How to play: </h3>
+                <p>
+                    Enter any 6-letter word that comes to mind. The corresponding boxes will be marked accordingly:
+                    <br /><br />
+                    🟩 – The letter is within any word in the anagram set.
+                    <br /><br />
+                    🟥 – The letter is NOT in the anagram set.
+                    <br /><br />
+                    🟨 – The letter is within any word in the anagram set, BUT of the wrong count (try thinking of words with more or less of this letter)
+                    <br /><br />
+                    Once you make a correct guess, come up with 2 anagrams for that word to win!
+                    <br /><br />
+                    <h4>Enjoy! 😊</h4>
+                </p>
+            </div>
             <div className="play-weldor">
                 <button onClick={onClose}>Play Weldor</button>
             </div>
