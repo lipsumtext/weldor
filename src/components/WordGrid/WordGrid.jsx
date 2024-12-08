ee1a90e2d52ae89f539e2ad94e92972535c373ca
silikeite <git@silikeite.me>
2024-12-08 23:37:48 +0800

    Shake on invalid word.

--- src/components/WordGrid/WordGrid.jsx
+++ src/components/WordGrid/WordGrid.jsx
@@ -1,26 +1,29 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useWeldor } from "../../hooks/useWeldor"
 import './WordGrid.styles.css'
 
-const Row = ({ word, savedWord, boxStatus, isActive }) => {
+const Row = ({ word, savedWord, boxStatus, isActive, isShake }) => {
+    let toShake = ''
+    if (isShake) toShake = ' shake'
     return (
-        <div className="row-container">
+        <div className={"row-container"}>
             {Array.from({ length: 6 }, (_, i) => (
-                <div key={i} className={boxStatus[i] || 'letter-box'}>
-                    { isActive ? word[i] || '' : savedWord[i]}
+                <div key={i} className={(boxStatus[i] || 'letter-box')+toShake}>
+                    {isActive ? word[i] || '' : savedWord[i]}
                 </div>
             ))}
         </div>
     )
 }
 
-export const WordGrid = ({rulesModalActive, weldorInstance}) => {
-    const { 
-        guessedWord, 
-        guessedWordSet, 
-        boxStatusSet, 
-        activeBoxKey, 
-        handleUserInput
+export const WordGrid = ({ rulesModalActive, weldorInstance }) => {
+    const {
+        guessedWord,
+        guessedWordSet,
+        boxStatusSet,
+        activeBoxKey,
+        handleUserInput,
+        shake
     } = weldorInstance
 
     useEffect(() => {
@@ -29,13 +32,20 @@ export const WordGrid = ({rulesModalActive, weldorInstance}) => {
         return () => window.removeEventListener('keyup', handleUserInput)
     }, [handleUserInput])
 
+    const [toShake, setToShake] = useState(false)
+
+    useEffect(() => {
+        setToShake(shake)
+    }, [shake])
+
     return (
         <div className="container">
             {Array.from({ length: 10 }, (_, i) => (
-                <Row word={guessedWord.toUpperCase()} 
-                    savedWord={guessedWordSet[i]} 
-                    boxStatus={boxStatusSet[i]} 
+                <Row word={guessedWord.toUpperCase()}
+                    savedWord={guessedWordSet[i]}
+                    boxStatus={boxStatusSet[i]}
                     isActive={i === activeBoxKey}
+                    isShake={toShake && (i === activeBoxKey)}
                 />
             ))}
         </div>
